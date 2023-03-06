486162b5bb126a1776b764267bae8c6d0fe04215
jorgeiglopez <jorgeiglopez@gmail.com>
2023-03-06 21:06:18 +1100

    Initial commit - Spring boot initializr for backend

---
+++ backend/src/main/java/com/jorgeiglopez/expensecoach/ExpenseCoachApplication.java
@@ -0,0 +1,13 @@
+package com.jorgeiglopez.expensecoach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseCoachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseCoachApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jorgeiglopez/expensecoach/ExpenseCoachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jorgeiglopez.expensecoach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseCoachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
